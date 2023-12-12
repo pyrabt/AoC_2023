c31818b3bf5ff9899775ec7158c8cd789aea666d
Will Marringa <wmarringa@gmail.com>
2023-12-11 21:45:49 -0700

    Takin out the part 2 trash for day 8 and 10

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -39,15 +39,18 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(steps)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+pub fn part_two(input: &str) -> Option<u64> {
     let mut get_instruction = true;
+    let mut instructions = "".to_owned();
     let mut nodes: HashMap<String, (String, String)> = HashMap::new();
     for line in input.lines() {
         if line.is_empty() && get_instruction {
             get_instruction = false;
             continue;
         }
-        if !get_instruction {
+        if get_instruction {
+            instructions.push_str(line);
+        } else {
             let node_name = line[0..=2].to_owned();
             let node_left = line[7..=9].to_owned();
             let node_right = line[12..=14].to_owned();
@@ -55,21 +58,34 @@ pub fn part_two(input: &str) -> Option<u32> {
         }
     }
 
-    let a_nodes: Vec<_> = nodes.keys().filter(|k| k.to_owned().ends_with("A")).collect();
-    let num_a = a_nodes.len();
+    let a_nodes: Vec<_> = nodes
+        .keys()
+        .filter(|k| k.to_owned().ends_with("A"))
+        .collect();
     let mut total_steps: Vec<_> = vec![];
-    let mut count = 0;
-    for node in a_nodes {
-        count += 1;
-        println!("Node: {node} ({}/{})", count, num_a);
-        total_steps.push(get_steps(node.to_owned(), &nodes, 0, vec![]).unwrap());
+    let instruction_chars: Vec<char> = instructions.chars().collect();
+    for a_node in a_nodes.as_slice() {
+        let mut in_index = 0;
+        let mut steps = 0;
+        let mut current_node: String = a_node.parse().unwrap();
+        while !current_node.ends_with("Z") {
+            let instruction = instruction_chars[in_index];
+            if instruction == 'L' {
+                let left_node = nodes.get(&current_node).unwrap().0.to_owned();
+                current_node = left_node;
+            } else {
+                let right_node = nodes.get(&current_node).unwrap().1.to_owned();
+                current_node = right_node;
+            }
+            steps += 1;
+            in_index = (in_index + 1) % instruction_chars.len();
+        }
+        total_steps.push(steps);
     }
-
-    println!("{:?}", total_steps);
     Some(lcm(total_steps))
 }
 
-fn lcm(numbers: Vec<u32>) -> u32 {
+fn lcm(numbers: Vec<u64>) -> u64 {
     let mut temp = numbers.clone();
 
     // check all the same
@@ -88,39 +104,18 @@ fn lcm(numbers: Vec<u32>) -> u32 {
         }
 
         // Find lowest index
-        match temp.iter().enumerate().min_by(|(_, a), (_, b)| a.cmp(b)).map(|(index, _)| index) {
+        match temp
+            .iter()
+            .enumerate()
+            .min_by(|(_, a), (_, b)| a.cmp(b))
+            .map(|(index, _)| index)
+        {
             Some(idx) => {
                 temp[idx] = temp[idx] + numbers[idx];
             }
-            None => panic!("Not possible")
+            None => panic!("Not possible"),
         }
     }
-}
-
-fn get_steps(current_node: String, nodes: &HashMap<String, (String, String)>, steps: u32, mut visited: Vec<String>) -> Option<u32> {
-    let node = nodes.get(current_node.as_str()).unwrap();
-    if node.0.ends_with("Z") || node.1.ends_with("Z") {
-        return Some(steps + 1);
-    }
-
-    if visited.contains(&current_node) { return None; }
-
-    visited.push(current_node.clone());
-    let left = match get_steps(nodes.get(&current_node).unwrap().0.to_owned(), &nodes, steps + 1, visited.clone()) {
-        None => 0,
-        Some(val) => val
-    };
-
-    let right = match get_steps(nodes.get(&current_node).unwrap().1.to_owned(), &nodes, steps + 1, visited.clone()) {
-        None => 0,
-        Some(val) => val
-    };
-
-    if left > right {
-        return Some(left);
-    }
-
-    Some(right)
 }
 
 #[cfg(test)]
--- src/bin/10.rs
+++ src/bin/10.rs
@@ -5,7 +5,12 @@ advent_of_code::solution!(10);
 pub fn part_one(input: &str) -> Option<u32> {
     let mut map: Vec<Vec<String>> = vec![];
     for line in input.lines() {
-        map.push(line.split("").filter(|s| !s.is_empty()).map(|s| s.to_owned()).collect());
+        map.push(
+            line.split("")
+                .filter(|s| !s.is_empty())
+                .map(|s| s.to_owned())
+                .collect(),
+        );
     }
 
     // get start indices
@@ -21,7 +26,7 @@ pub fn part_one(input: &str) -> Option<u32> {
     let mut next: (usize, usize) = (0, 0);
     match start_next_pipe(s_yx, &map) {
         Some(val) => next = val[0],
-        None => println!("no path found")
+        None => println!("no path found"),
     };
 
     let mut last = s_yx;
@@ -32,12 +37,14 @@ pub fn part_one(input: &str) -> Option<u32> {
     while !dead_end {
         match get_next(current, last, &map) {
             Some(val) => next = val,
-            None => dead_end = true
+            None => dead_end = true,
         };
         steps.push(map[current.0][current.1].clone());
         last = current;
         current = next;
-        if current == s_yx { break; }
+        if current == s_yx {
+            break;
+        }
     }
 
     return Some(div_ceil(steps.len(), 2) as u32);
@@ -47,19 +54,27 @@ fn start_next_pipe(current: (usize, usize), map: &Vec<Vec<String>>) -> Option<Ve
     let mut options: Vec<(usize, usize)> = vec![];
     if current.1 > 0 {
         let lt_pipe = map[current.0][current.1 - 1].clone();
-        if lt_pipe.eq("-") || lt_pipe.eq("F") || lt_pipe.eq("L") { options.push((current.0, current.1 - 1)); }
+        if lt_pipe.eq("-") || lt_pipe.eq("F") || lt_pipe.eq("L") {
+            options.push((current.0, current.1 - 1));
+        }
     }
     if current.0 > 0 {
         let lt_pipe = map[current.0 - 1][current.1].clone();
-        if lt_pipe.eq("|") || lt_pipe.eq("F") || lt_pipe.eq("7") { options.push((current.0 - 1, current.1)); }
+        if lt_pipe.eq("|") || lt_pipe.eq("F") || lt_pipe.eq("7") {
+            options.push((current.0 - 1, current.1));
+        }
     }
     if current.0 < map.len() - 1 {
         let lt_pipe = map[current.0 + 1][current.1].clone();
-        if lt_pipe.eq("|") || lt_pipe.eq("L") || lt_pipe.eq("J") { options.push((current.0 + 1, current.1)); }
+        if lt_pipe.eq("|") || lt_pipe.eq("L") || lt_pipe.eq("J") {
+            options.push((current.0 + 1, current.1));
+        }
     }
     if current.1 < map[0].len() - 1 {
         let lt_pipe = map[current.0][current.1 + 1].clone();
-        if lt_pipe.eq("-") || lt_pipe.eq("J") || lt_pipe.eq("7") { options.push((current.0, current.1 + 1)); }
+        if lt_pipe.eq("-") || lt_pipe.eq("J") || lt_pipe.eq("7") {
+            options.push((current.0, current.1 + 1));
+        }
     }
     if map.len() > 0 {
         return Some(options);
@@ -68,45 +83,162 @@ fn start_next_pipe(current: (usize, usize), map: &Vec<Vec<String>>) -> Option<Ve
     None
 }
 
-fn get_next(current: (usize, usize), last: (usize, usize), map: &Vec<Vec<String>>) -> Option<(usize, usize)> {
+fn get_next(
+    current: (usize, usize),
+    last: (usize, usize),
+    map: &Vec<Vec<String>>,
+) -> Option<(usize, usize)> {
     let pipe_type = map[current.0][current.1].clone();
     if pipe_type.eq("|") {
         //up
-        if last.0 != current.0 - 1 && current.0 > 0 { return Some((current.0 - 1, current.1)); }
+        if last.0 != current.0 - 1 && current.0 > 0 {
+            return Some((current.0 - 1, current.1));
+        }
         //down
-        if current.0 < map.len() - 1 { return Some((current.0 + 1, current.1)); }
+        if current.0 < map.len() - 1 {
+            return Some((current.0 + 1, current.1));
+        }
     } else if pipe_type.eq("-") {
         //left
-        if last.1 != current.1 - 1 && current.1 > 0 { return Some((current.0, current.1 - 1)); }
+        if last.1 != current.1 - 1 && current.1 > 0 {
+            return Some((current.0, current.1 - 1));
+        }
         //right
-        if current.1 < map[0].len() - 1 { return Some((current.0, current.1 + 1)); }
+        if current.1 < map[0].len() - 1 {
+            return Some((current.0, current.1 + 1));
+        }
     } else if pipe_type.eq("7") {
         //left
-        if last.1 != current.1 - 1 && current.1 > 0 { return Some((current.0, current.1 - 1)); }
+        if last.1 != current.1 - 1 && current.1 > 0 {
+            return Some((current.0, current.1 - 1));
+        }
         //down
-        if current.0 < map.len() - 1 { return Some((current.0 + 1, current.1)); }
+        if current.0 < map.len() - 1 {
+            return Some((current.0 + 1, current.1));
+        }
     } else if pipe_type.eq("L") {
         //up
-        if last.0 != current.0 - 1 && current.0 > 0 { return Some((current.0 - 1, current.1)); }
+        if last.0 != current.0 - 1 && current.0 > 0 {
+            return Some((current.0 - 1, current.1));
+        }
         //right
-        if current.1 < map[0].len() - 1 { return Some((current.0, current.1 + 1)); }
+        if current.1 < map[0].len() - 1 {
+            return Some((current.0, current.1 + 1));
+        }
     } else if pipe_type.eq("J") {
         //up
-        if last.0 != current.0 - 1 && current.0 > 0 { return Some((current.0 - 1, current.1)); }
+        if last.0 != current.0 - 1 && current.0 > 0 {
+            return Some((current.0 - 1, current.1));
+        }
         //left
-        if current.1 > 0 { return Some((current.0, current.1 - 1)); }
+        if current.1 > 0 {
+            return Some((current.0, current.1 - 1));
+        }
     } else if pipe_type.eq("F") {
         //right
-        if last.1 != current.1 + 1 && current.1 < map[0].len() - 1 { return Some((current.0, current.1 + 1)); }
+        if last.1 != current.1 + 1 && current.1 < map[0].len() - 1 {
+            return Some((current.0, current.1 + 1));
+        }
         //down
-        if current.0 < map.len() - 1 { return Some((current.0 + 1, current.1)); }
+        if current.0 < map.len() - 1 {
+            return Some((current.0 + 1, current.1));
+        }
     }
 
     None
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut map: Vec<Vec<String>> = vec![];
+    for line in input.lines() {
+        map.push(
+            line.split("")
+                .filter(|s| !s.is_empty())
+                .map(|s| s.to_owned())
+                .collect(),
+        );
+    }
+
+    // get start indices
+    let mut s_yx = (0, 0);
+    for l in 0..map.len() {
+        if map[l].contains(&"S".to_owned()) {
+            s_yx = (l, map[l].iter().position(|x| x == &"S".to_owned()).unwrap());
+            break;
+        }
+    }
+
+    let mut current: (usize, usize);
+    let mut next: (usize, usize) = (0, 0);
+    match start_next_pipe(s_yx, &map) {
+        Some(val) => next = val[0],
+        None => println!("no path found"),
+    };
+
+    let mut last = s_yx;
+    let mut loop_ind: Vec<(usize, usize)> = vec![];
+    loop_ind.push(s_yx);
+    current = next;
+
+    let mut dead_end = false;
+    while !dead_end {
+        match get_next(current, last, &map) {
+            Some(val) => next = val,
+            None => dead_end = true,
+        };
+        loop_ind.push((current.0, current.1));
+        last = current;
+        current = next;
+        if current == s_yx {
+            break;
+        }
+    }
+
+    let points_len = loop_ind.len();
+    for i in &loop_ind {
+        map[i.0][i.1] = "X".to_owned();
+    }
+
+    let mut edges: Vec<((i64, i64), (i64, i64))> = vec![];
+    for i in 0..points_len {
+        if i == points_len - 1 {
+            edges.push((
+                (loop_ind[i].0 as i64, loop_ind[i].1 as i64),
+                (loop_ind[0].0 as i64, loop_ind[0].1 as i64),
+            ));
+        } else {
+            edges.push((
+                (loop_ind[i].0 as i64, loop_ind[i].1 as i64),
+                (loop_ind[i + 1].0 as i64, loop_ind[i + 1].1 as i64),
+            ));
+        }
+    }
+
+    let mut total: u32 = 0;
+    for y in 0..map.len() {
+        for x in 0..map[0].len() {
+            if map[y][x] != "X".to_owned() {
+                if is_inside(&edges, (y as i64, x as i64)) {
+                    total += 1;
+                }
+            }
+        }
+    }
+
+    Some(total)
+}
+
+//Ray casting algorithm
+fn is_inside(edges: &Vec<((i64, i64), (i64, i64))>, point: (i64, i64)) -> bool {
+    let (yp, xp) = point;
+    let mut count = 0;
+    for edge in edges {
+        let ((y1, x1), (y2, x2)) = edge;
+        if (yp < *y1) != (yp < *y2) && xp < (x1 + ((yp - y1) / (y2 - y1)) * (x2 - x1)) {
+            count += 1;
+        }
+    }
+    count % 2 == 1
 }
 
 #[cfg(test)]
@@ -122,6 +254,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(8));
     }
 }
